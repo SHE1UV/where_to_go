50484376dc42931d24af56405c3a0d7cd24f39e5
SHE1UV <126342687+SHE1UV@users.noreply.github.com>
2025-07-10 10:57:37 +0200

    Update models.py

--- places/models.py
+++ places/models.py
@@ -3,38 +3,38 @@ from tinymce.models import HTMLField
 
 
 class Place(models.Model):
-    title = models.CharField('Название', max_length=200)
-    short_description = models.TextField('Краткое описание', blank=True)
-    long_description = HTMLField('Описание', blank=True)
-    latitude = models.FloatField('Широта')
-    longitude = models.FloatField('Долгота')
+    title = models.CharField("Название", max_length=200)
+    short_description = models.TextField("Краткое описание", blank=True)
+    long_description = HTMLField("Описание", blank=True)
+    latitude = models.FloatField("Широта")
+    longitude = models.FloatField("Долгота")
 
     class Meta:
-        verbose_name = 'Место'
-        verbose_name_plural = 'Места'
+        verbose_name = "Место"
+        verbose_name_plural = "Места"
 
     def __str__(self):
         return self.title
 
 
 class Image(models.Model):
-    image = models.ImageField('Картинки')
+    image = models.ImageField("Картинки")
     place = models.ForeignKey(
         Place,
         on_delete=models.CASCADE,
-        verbose_name='Место',
-        related_name='images'
+        verbose_name="Место",
+        related_name="images"
     )
     ordinal_number = models.PositiveIntegerField(
-        'Порядковый номер',
+        "Порядковый номер",
         default=0,
         db_index=True,
     )
 
     class Meta:
-        verbose_name = 'Картинка'
-        verbose_name_plural = 'Картинки'
-        ordering = ['ordinal_number']
+        verbose_name = "Картинка"
+        verbose_name_plural = "Картинки"
+        ordering = ["ordinal_number"]
 
     def __str__(self):
-        return f'{self.ordinal_number} {self.place.title}'
+        return f"{self.ordinal_number} {self.place.title}"
