f4504543aa38058aa60a7c1e9baee2eb35403875
SHE1UV <126342687+SHE1UV@users.noreply.github.com>
2025-07-10 09:45:11 +0200

    Update urls.py

--- where_to_go/urls.py
+++ where_to_go/urls.py
@@ -9,7 +9,7 @@ from where_to_go import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.show_main_page),
-    path('places/<int:place_id>', views.fetch_place_details)
+    path('places/<int:place_id>/', views.fetch_place_details)
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
