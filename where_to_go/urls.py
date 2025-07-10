2e0fa27f7ca8c9e97cc16a34e453fe6a19a7a6af
SHE1UV <126342687+SHE1UV@users.noreply.github.com>
2025-07-10 09:48:03 +0200

    Update urls.py

--- where_to_go/urls.py
+++ where_to_go/urls.py
@@ -3,13 +3,13 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
 
-from where_to_go import views
+from places import views
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.show_main_page),
-    path('places/<int:place_id>/', views.fetch_place_details)
+    path('', views.show_main_page, name='main_page'),
+    path('places/<int:place_id>/', views.fetch_place_details, name='place_details'),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
