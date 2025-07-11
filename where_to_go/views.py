164860d7869420e27f9a8c36264b047ba737b693
SHE1UV <126342687+SHE1UV@users.noreply.github.com>
2025-07-11 17:32:36 +0200

    Update views.py

--- where_to_go/views.py
+++ where_to_go/views.py
@@ -3,6 +3,34 @@ from django.http import JsonResponse
 from places.models import Place
 
 
+def show_main_page(request):
+    places = Place.objects.all()
+
+    features = [
+        {
+            "type": "Feature",
+            "geometry": {
+                "type": "Point",
+                "coordinates": [place.longitude, place.latitude]
+            },
+            "properties": {
+                "title": place.title,
+                "placeId": place.id,
+                "detailsUrl": reverse('place_details', kwargs={"place_id": place.id})
+            }
+        } for place in places
+    ]
+
+    context = {
+        "places": {
+            "type": "FeatureCollection",
+            "features": features
+        }
+    }
+
+    return render(request, "index.html", context)
+
+
 def fetch_place_details(request, place_id):
     place = get_object_or_404(
         Place.objects.prefetch_related("images"),
