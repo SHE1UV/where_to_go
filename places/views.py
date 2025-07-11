4ec96f2ac9108cfef1b6d54f1923e9d552e0b709
SHE1UV <126342687+SHE1UV@users.noreply.github.com>
2025-07-11 17:41:33 +0200

    Update views.py

--- places/views.py
+++ places/views.py
@@ -1,1 +1,26 @@
+from django.shortcuts import get_object_or_404
+from django.http import JsonResponse
+from .models import Place
+
+
+def fetch_place_details(request, place_id):
+    place = get_object_or_404(
+        Place.objects.prefetch_related("images"),
+        id=place_id
+    )
+
+    images_urls = [image.image.url for image in place.images.all()]
+
+    payload = {
+        "title": place.title,
+        "imgs": images_urls,
+        "short_description": place.short_description,
+        "long_description": place.long_description,
+        "coordinates": {
+            "lng": place.longitude,
+            "lat": place.latitude,
+        }
+    }
+
+    return JsonResponse(payload, json_dumps_params={"ensure_ascii": False})
 
